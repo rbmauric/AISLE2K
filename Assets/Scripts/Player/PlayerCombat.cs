fad6c8a2dbf5a51080f21d95b776b54e06ec2c30
rbmauric <35748166+rbmauric@users.noreply.github.com>
2020-06-19 13:47:33 -0700

    Source Code
    Added source code (reusing previous scripts but added incrementally to avoid conflicts)

---
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    //Hitboxes
+    public GameObject attackHitBox;
+    public GameObject kickHitbox;
+
+    //Damage Numbers
+    public int attackDamage = 20;
+    public int kickDamage = 40;
+
+    //Timers
+    public float attackActive = 0.7f;
+    public float startUp = 0.2f;
+    public float reset = 1f;
+
+    public float attackSpeed = 0.2f;
+    public float kickSpeed = 0.7f;
+
+    //Coroutine Controllers
+    public bool isAttacking = false;
+    public bool isReset = false;
+
+    //Misc
+    public int attackCount = 0;
+    private Animator animator;
+    private PlayerMovement pm;
+
+    private void Start()
+    {
+        attackHitBox.SetActive(false);
+        kickHitbox.SetActive(false);
+        animator = GetComponent<Animator>();
+        pm = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Primary Attack
+        if (Input.GetButtonDown("Fire1") && !isAttacking && pm.groundCheck)
+        {
+            animator.SetFloat("Speed", 0);
+            pm.canMove = false;
+
+            isAttacking = true;
+            StartCoroutine(Attack());
+        }
+        else if (Input.GetButtonDown("Fire2") && !isAttacking && pm.groundCheck)
+        {
+            animator.SetFloat("Speed", 0);
+            pm.canMove = false;
+
+            isAttacking = true;
+            attackCount = 2;
+            StartCoroutine(Attack());
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (isReset)
+        {
+            StartCoroutine(Reset());
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        //Coroutine Limiter and Startup Window
+        isAttacking = true;
+        animator.SetTrigger("Attack");
+        yield return new WaitForSeconds(startUp);
+   
+        //Do the attack
+        if (attackCount < 2)
+        {
+            attackCount++;
+            
+            attackHitBox.SetActive(true);
+            yield return new WaitForSeconds(attackActive);
+            attackHitBox.SetActive(false);
+
+            yield return new WaitForSeconds(attackSpeed);
+        }
+        else
+        {
+            kickHitbox.SetActive(true);
+            attackCount = 0;
+            yield return new WaitForSeconds(attackActive);
+            kickHitbox.SetActive(false);
+
+            yield return new WaitForSeconds(kickSpeed);
+        }
+
+        //Resetting
+        isAttacking = false;
+        pm.canMove = true;
+        isReset = true;
+    }
+
+    IEnumerator Reset()
+    {
+        isReset = false;
+        //Debug.Log("Reset");
+
+        yield return new WaitForSeconds(reset);
+
+        if (isAttacking)
+        {
+            yield break;
+        }
+        else
+        {
+            //Debug.Log("Reset Attack Counter");
+            attackCount = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Movement Values
+    public float speed = 5;
+    public float jumpVelocity;
+    public float moveTime = 0.2f;
+
+    //Controllers
+    public bool right;
+    public bool groundCheck;
+    public bool canFlip;
+    public bool canMove;
+    private bool moving = false;
+
+    //Misc
+    private Animator animator;
+    //public SoundManager sm;
+    //public float soundLoop = 0.3f;
+    //private bool soundActive;
+
+    private void Start()
+    {
+        canMove = true;
+        right = true;
+        animator = GetComponentInParent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            playerMovement();
+            Jump();
+            Flip();
+        }
+    }
+
+    void playerMovement()
+    {
+        if (Input.GetButton("Horizontal") && canMove)
+        {
+            moving = true;
+            animator.SetFloat("Speed", speed);
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f);
+            transform.position += movement * Time.deltaTime * speed;
+        }
+        else
+        {
+            moving = false;
+            animator.SetFloat("Speed", 0);
+        }
+    }
+
+    void Flip()
+    {
+        if (Input.GetAxis("Horizontal") > 0 && !right || Input.GetAxis("Horizontal") < 0 && right)
+        {
+            right = !right;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && groundCheck == true)
+        {
+            groundCheck = false;
+            animator.SetBool("Jump", true);
+            GetComponentInParent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+        }
+    }
+}
